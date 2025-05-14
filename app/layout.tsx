6d459e2be6e19966a9e6dea18ea8a263f6bc2a03
Muhammad Dwiky Cahyo Wicaksono <mdwikycahyo@gmail.com>
2025-05-14 09:00:53 +0700

    rename v0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Hypearn",
+  description: "Hypearn",
+  generator: "Hypearn.dev",
 }
 
 export default function RootLayout({
